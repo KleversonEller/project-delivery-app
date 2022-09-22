e387fe273847bcee30872dea6d4d8ef016580f33
Raphael <rafa.ramos_22@hotmail.com>
2022-09-22 11:46:15 -0300

    fix: corrigind o jwt

--- back-end/src/utils/jwt.js
+++ back-end/src/utils/jwt.js
@@ -4,7 +4,7 @@ const { StatusCodes } = require('http-status-codes');
 const throwMyError = require('./throwMyError');
 
 const createToken = ({ email, role }) => {
-  const token = jwt.sign({ data: { email, role } }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ data: { email, role } }, process.env.JWT_SECRET || 'secret_key', {
     expiresIn: '30d',
       algorithm: 'HS256',
   });
@@ -13,7 +13,7 @@ const createToken = ({ email, role }) => {
 
 const checkToken = (token) => {
   try {
-    const { data } = jwt.verify(token, process.env.JWT_SECRET);
+    const { data } = jwt.verify(token, process.env.JWT_SECRE || 'secret_key');
     return data;
   } catch (_err) {
     throwMyError(StatusCodes.UNAUTHORIZED, 'Token expirado ou inválido');
