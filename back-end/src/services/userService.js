a0d16f4661cb4577d601e23c0959998ef1bbf2fe
Raphael <rafa.ramos_22@hotmail.com>
2022-09-22 03:39:36 -0300

    feat: adicionando userSevice com as validaçoes para criar usuario

---
+++ back-end/src/services/userService.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+const { StatusCodes } = require('http-status-codes');
+const { users } = require('../database/models/index');
+const { encryptPassword } = require('../utils/md5')
+const { createToken } = require('../utils/jwt');
+const throwMyError = require('../utils/throwMyError');
+
+const userSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+const createUser = async (user) => {
+  console.log(user.email)
+  const { error } = userSchema.validate(user);
+
+  if (error) throwMyError(StatusCodes.BAD_REQUEST, 'Dados inválidos');
+
+  const { password } = user;
+  const passwordHash = encryptPassword(password);
+
+  if (await users.findOne({ where: { email: user.email } })) {
+    throwMyError(StatusCodes.CONFLICT, 'O usuário já possui cadastro');
+  }
+  const newUser = await users.create({ ...user, password: passwordHash, role: 'customer' });
+  const token = createToken({ email: user.email, role: 'customer' });
+  return {
+    token,
+    role: newUser.role,
+  };
+};
+
+module.exports = { 
+  createUser,
+};
+ 
