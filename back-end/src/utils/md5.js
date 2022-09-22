08c856706a393c7a30b98032c44eb7e10754d46d
Raphael <rafa.ramos_22@hotmail.com>
2022-09-22 03:38:09 -0300

    feat: adicionando md5 com a funçao de criptografa o password

---
+++ back-end/src/utils/md5.js
@@ -0,0 +1,10 @@
+const md5 = require('md5');
+
+const encryptPassword = (password) => {
+  const passwordHash = md5(password);
+  return passwordHash;
+};
+
+module.exports = {
+ encryptPassword,
+}
