26c40a132b39c821c7c371ff6309e6159dd085a5
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-22 23:25:19 -0300

    feat: criada page Customer

--- front-end/src/pages/Customer.jsx
+++ front-end/src/pages/Customer.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import Header from '../components/Header';
+import MyContext from '../contexts/MyContext';
+import Footer from '../components/Footer';
+import CardProduct from '../components/CardProduct';
+
+import requestGetAllProducts from '../services/requestGetAllProducts';
 
 function Customer() {
+  const { setIsFetching } = useContext(MyContext);
+  const [products, setProducts] = useState([]);
+  const [shoppingCart, setShoppingCart] = useState([]);
+
+  useEffect(() => {
+    setShoppingCart(JSON.parse(localStorage.getItem('carrinho')) || []);
+    const getAllProducts = async () => {
+      setIsFetching(true);
+      const result = await requestGetAllProducts();
+      setProducts(result);
+      setIsFetching(false);
+    };
+    getAllProducts();
+  }, [setIsFetching]);
+
   return (
-    <h1>Produtos</h1>
+    <div>
+      <Header title="Produtos" />
+      <div>
+        {
+          products?.map((element, i) => (
+            (
+              <CardProduct
+                element={ element }
+                i={ i }
+                key={ element.name }
+                shoppingCart={ shoppingCart }
+                setShoppingCart={ setShoppingCart }
+              />
+            )
+          ))
+        }
+      </div>
+      <Footer
+        existeFooter="true"
+        shoppingCart={ shoppingCart }
+      />
+    </div>
   );
 }
 
