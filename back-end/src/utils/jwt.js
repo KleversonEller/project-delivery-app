952ac076951527b63e9c9b1a87c26b6b38a43015
Italo <contatoitallo@hotmail.com>
2022-09-23 07:36:10 -0300

    style:

--- back-end/src/utils/jwt.js
+++ back-end/src/utils/jwt.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
-const fs = require('fs')
+const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const { StatusCodes } = require('http-status-codes');
 const throwMyError = require('./throwMyError');
+
 const senha = fs.readFileSync('jwt.evaluation.key');
 
 const createToken = ({ email, role }) => {
