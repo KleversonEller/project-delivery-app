94fe776cc9671abc08c85a0625ce68c6a479782e
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-22 23:26:20 -0300

    chore: criada rota "/customer/checkout"

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,14 +4,16 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Customer from './pages/Customer';
 import Register from './pages/Register';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route exact path="/login" component={ Login } />
-      <Route exact path="/customer/products" component={ Customer } />
       <Route exact path="/register" component={ Register } />
+      <Route exact path="/customer/products" component={ Customer } />
+      <Route exact path="/customer/checkout" component={ Checkout } />
     </Switch>
   );
 }
