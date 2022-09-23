263fb02a3fdcc75ff10d8ca81e0b6336d474d97b
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-22 23:18:27 -0300

    feat: criado componente CardFood

---
+++ front-end/src/components/CardProduct.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import convertToBrasilianCurrency from '../helpers/convertToBrasilianCurrency';
+import manualOperationProduct
+  from '../helpers/manualOperationProductToShoppingCart';
+import operationProduct from '../helpers/operationProduct';
+import saveProductAtShoppingCart from '../helpers/saveProductAtShoppingCart';
+// import { Link } from 'react-router-dom';
+
+export default function CardFood(props) {
+  const { element } = props;
+  const { name, price, urlImage, id } = element;
+  const { shoppingCart, setShoppingCart } = props;
+  const [quantity, setQuantity] = useState(0);
+
+  useEffect(() => {
+    const findProductIndex = shoppingCart
+      .findIndex((prod) => prod.id === id);
+
+    if (findProductIndex >= 0) {
+      setQuantity(shoppingCart[findProductIndex].quantity);
+    }
+  }, [shoppingCart, id]);
+
+  useEffect(() => {
+    setShoppingCart(JSON.parse(localStorage.getItem('carrinho')) || []);
+  }, [quantity, setShoppingCart]);
+
+  const handleClick = (operation) => {
+    const newValue = operationProduct(quantity, operation);
+    saveProductAtShoppingCart(element, newValue);
+    setQuantity(newValue);
+  };
+
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    const newValue = manualOperationProduct(value);
+
+    if (newValue !== null) {
+      saveProductAtShoppingCart(element, newValue);
+      setQuantity(newValue);
+    }
+  };
+
+  return (
+    // <Link
+    //   to={ `/foods/${idMeal}` }
+    //   className="card_container"
+    //   style={ { textDecoration: 'none' } }
+    // >
+    <div>
+      <p data-testid={ `customer_products__element-card-title-${id}` }>
+        {name}
+      </p>
+      <img
+        data-testid={ `customer_products__img-card-bg-image-${id}` }
+        src={ urlImage }
+        alt="search"
+        style={ { width: '50px' } }
+      />
+      <p data-testid={ `customer_products__element-card-price-${id}` }>
+        R$
+        {convertToBrasilianCurrency(price)}
+      </p>
+
+      <button
+        data-testid={ `customer_products__button-card-rm-item-${id}` }
+        type="button"
+        onClick={ () => handleClick('-') }
+      >
+        -
+      </button>
+
+      <input
+        data-testid={ `customer_products__input-card-quantity-${id}` }
+        value={ quantity }
+        onChange={ handleChange }
+      />
+
+      <button
+        data-testid={ `customer_products__button-card-add-item-${id}` }
+        type="button"
+        onClick={ () => handleClick('+') }
+      >
+        +
+      </button>
+    </div>
+    // </Link>
+  );
+}
+
+CardFood.propTypes = {
+  strMeal: PropTypes.string,
+  strMealThumb: PropTypes.string,
+}.isRequired;
