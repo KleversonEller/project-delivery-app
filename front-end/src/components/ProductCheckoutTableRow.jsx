e05ab0212ba26d334ca962ce24bc620a51b66a6e
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-23 21:34:57 -0300

    feat: criado componente que representa a linha da tabela de checkout de produtos

--- front-end/src/components/ProductCheckoutTableRow.jsx
+++ front-end/src/components/ProductCheckoutTableRow.jsx
@@ -1,34 +1,58 @@
-import React, { useEffect, useHistory } from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import convertToBrazilianCurrency from '../helpers/convertToBrazilianCurrency';
 
 function ProductCheckoutTableRow(props) {
-  const { history: { location: { pathname } } } = useHistory();
-  const { element, setShoppingCart } = props;
+  const { location: { pathname } } = useHistory();
+  const { element, setShoppingCart, i } = props;
   const { id, name, price, quantity } = element;
-
-  useEffect(() => (
-    setShoppingCart(JSON.parse(localStorage.getItem(carrinho)))
-  ), [setShoppingCart]);
+  const carrinho = 'carrinho';
 
   const removeProduct = () => {
-    const carrinho = 'carrinho';
     const localStorageProducts = JSON.parse(localStorage.getItem(carrinho));
     const filterProducts = localStorageProducts.filter((product) => product.id !== id);
 
     localStorage.setItem(carrinho, JSON.stringify(filterProducts));
+    setShoppingCart(JSON.parse(localStorage.getItem(carrinho)));
   };
 
   return (
     <tr>
-      <td>{id}</td>
-      <td>{name}</td>
-      <td>{quantity}</td>
-      <td>{price}</td>
-      <td>{convertToBrazilianCurrency(quantity * price)}</td>
-      {(pathname.includes('/customer/checkout')) && (
+      <td
+        data-testid={ `customer_checkout__element-order-table-item-number-${i}` }
+      >
+        {i + 1}
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-name-${i}` }
+      >
+        {name}
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-quantity-${i}` }
+      >
+        {quantity}
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-unit-price-${i}` }
+      >
+        {price}
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-sub-total-${i}` }
+      >
+        {convertToBrazilianCurrency(quantity * price)}
+      </td>
+      {(pathname === '/customer/checkout') && (
         <td>
-          <button type="button" onClick={ removeProduct }>Remover</button>
+          <button
+            type="button"
+            onClick={ removeProduct }
+            data-testid={ `customer_checkout__element-order-table-remove-${i}` }
+          >
+            Remover
+          </button>
         </td>
       )}
     </tr>
@@ -39,10 +63,11 @@ ProductCheckoutTableRow.propTypes = {
   element: PropTypes.shape({
     id: PropTypes.number,
     name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
+    price: PropTypes.string.isRequired,
     quantity: PropTypes.number.isRequired,
   }).isRequired,
   setShoppingCart: PropTypes.func.isRequired,
+  i: PropTypes.number.isRequired,
 };
 
 export default ProductCheckoutTableRow;
