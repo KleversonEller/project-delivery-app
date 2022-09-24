0e15b4789dc736e22040550008c8b2e4802f066d
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-23 21:37:23 -0300

    feat: adicionado componente com as informações dos detalhes de checkout de produtos

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TabelaDespesas from '../components/TabelaDespesas';
 import Header from '../components/Header';
-import ProductCheckoutTableaddressDetails from './ProductCheckoutTableaddressDetails';
+import ProductCheckoutTableaddressDetails
+  from '../components/ProductCheckoutTableaddressDetails';
+import requestGetAllSellers from '../services/requestGetAllSellers';
 
 function Checkout() {
+  const [sellers, setSellers] = useState([]);
+  const [requestError, setRequestError] = useState('');
+
+  console.log(requestError);
+  console.log(sellers);
+
+  useEffect(() => {
+    const getSellers = async () => {
+      const sellerList = await requestGetAllSellers();
+
+      if (sellerList.message) return setRequestError(sellerList.message);
+      setSellers(sellerList);
+    };
+    getSellers();
+  }, []);
+
   return (
     <div>
       <Header title="Produtos" />
       <h1>Finalizar Pedido</h1>
       <TabelaDespesas />
       <h1>Detalhes e Endereço para Entrega</h1>
-      <ProductCheckoutTableaddressDetails />
+      <ProductCheckoutTableaddressDetails
+        sellers={ sellers }
+        user={ user }
+      />
     </div>
   );
 }
