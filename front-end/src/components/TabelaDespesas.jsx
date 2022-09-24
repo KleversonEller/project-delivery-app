8705637416fb73454fecdfa60cc5e950c28ece78
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-23 21:36:17 -0300

    feat: criado componentes com a tabela de informações de checkout de produtos

--- front-end/src/components/TabelaDespesas.jsx
+++ front-end/src/components/TabelaDespesas.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import ProductCheckoutTableRow from './ProductCheckoutTableRow';
+import calculatesTotalPrice from '../helpers/calculatesTotalPrice';
 
 export default function TabelaDespesas() {
   const [shoppingCart, setShoppingCart] = useState([]);
+
   useEffect(() => {
     setShoppingCart(JSON.parse(localStorage.getItem('carrinho')) || []);
   }, []);
@@ -10,25 +12,37 @@ export default function TabelaDespesas() {
   return (
     <div>
       <table>
-        <tr>
-          <th>Item</th>
-          <th>Descrição</th>
-          <th>Quabtidade</th>
-          <th>Valor Unitário</th>
-          <th>Sub-total</th>
-          <th>Remover Item</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quabtidade</th>
+            <th>Valor Unitário</th>
+            <th>Sub-total</th>
+            <th>Remover Item</th>
+          </tr>
+        </thead>
 
-        {shoppingCart?.map((element, i) => (
-          <ProductCheckoutTableRow
-            key={ i }
-            element={ element }
-            shoppingCart={ shoppingCart }
-            setShoppingCart={ setShoppingCart }
-          />
-        ))}
+        <tbody>
+          {shoppingCart?.map((element, i) => (
+            <ProductCheckoutTableRow
+              key={ i }
+              element={ element }
+              shoppingCart={ shoppingCart }
+              setShoppingCart={ setShoppingCart }
+              calculatesTotalPrice={ calculatesTotalPrice }
+              i={ i }
+            />
+          ))}
+        </tbody>
 
       </table>
+      <p
+        data-testid="customer_checkout__element-order-total-price"
+      >
+        {Number(calculatesTotalPrice(shoppingCart).toFixed(2))}
+
+      </p>
     </div>
   );
 }
