d9751cdee87e327ae5e539aa2b019286d0173c40
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-25 19:59:36 -0300

    chore: propriedade "sallerId" alterada para "sellerId"

--- back-end/src/database/models/sales.js
+++ back-end/src/database/models/sales.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       foreignKey: true,
     },
-    sallerId: {
+    sellerId: {
       type: DataTypes.INTEGER,
       foreignKey: true,
     },
