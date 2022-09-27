5f5941e40a052878aec948e75d34cf35f93ab6aa
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-26 22:10:29 -0300

    fix: adicionado await

--- front-end/src/services/requestGetAllSales.js
+++ front-end/src/services/requestGetAllSales.js
@@ -9,7 +9,7 @@ const requestGetAllSales = async (token) => {
         Authorization: token,
       },
     });
-    const result = response.json();
+    const result = await response.json();
 
     return result;
   } catch (err) {
