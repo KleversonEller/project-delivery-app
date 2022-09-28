abcc1f205e4808fff3b99f783c481436720c2395
Kleverson Eller <kleverson.a16@gmail.com>
2022-09-28 20:06:20 -0300

    style: :lipstick: Implementando correçoes no CSS para manage

--- front-end/src/pages/Manage.jsx
+++ front-end/src/pages/Manage.jsx
@@ -46,69 +46,68 @@ function Manage() {
   };
 
   return (
-    <>
+    <div className="w-screen h-screen flex flex-col items-center">
       <Header />
-      <div className="w-screen h-screen flex items-center justify-center">
-        <form
-          className={ `bg-zinc-300 flex flex-col h-3/4
-        w-2/5 rounded items-center py-20 justify-between` }
-          onSubmit={ handleSubmit }
+      <form
+        className={ `bg-zinc-300 flex flex-col h-3/4
+        w-2/5 rounded items-center py-20 justify-between mt-10` }
+        onSubmit={ handleSubmit }
+      >
+        <Input
+          className="bg-zinc-400 rounded h-8 placeholder:text-zinc-600 p-3"
+          type="text"
+          data-testid="admin_manage__input-name"
+          id="name"
+          placeholder="Name"
+          value={ name }
+          onChange={ ({ target }) => setName(target.value) }
+        />
+        <Input
+          className="bg-zinc-400 rounded h-8 placeholder:text-zinc-600 p-3"
+          type="text"
+          data-testid="admin_manage__input-email"
+          id="email"
+          placeholder="Email"
+          value={ email }
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
+        <Input
+          className="bg-zinc-400 rounded h-8 placeholder:text-zinc-600 p-3"
+          type="password"
+          data-testid="admin_manage__input-password"
+          id="password"
+          placeholder="Password"
+          value={ password }
+          onChange={ ({ target }) => setPassword(target.value) }
+        />
+        <select
+          className="bg-zinc-400 rounded h-8 px-3 w-1/2"
+          data-testid="admin_manage__select-role"
+          id="role"
+          type="role"
+          onChange={ ({ target }) => setRole(target.value) }
         >
-          <Input
-            className="bg-zinc-400 rounded h-8 placeholder:text-zinc-600 p-3"
-            type="text"
-            data-testid="admin_manage__input-name"
-            id="name"
-            placeholder="Name"
-            value={ name }
-            onChange={ ({ target }) => setName(target.value) }
-          />
-          <Input
-            className="bg-zinc-400 rounded h-8 placeholder:text-zinc-600 p-3"
-            type="text"
-            data-testid="admin_manage__input-email"
-            id="email"
-            placeholder="Email"
-            value={ email }
-            onChange={ ({ target }) => setEmail(target.value) }
-          />
-          <Input
-            className="bg-zinc-400 rounded h-8 placeholder:text-zinc-600 p-3"
-            type="password"
-            data-testid="admin_manage__input-password"
-            id="password"
-            placeholder="Password"
-            value={ password }
-            onChange={ ({ target }) => setPassword(target.value) }
-          />
-          <select
-            data-testid="admin_manage__select-role"
-            id="role"
-            type="role"
-            onChange={ ({ target }) => setRole(target.value) }
-          >
-            <option value="customer">Cliente</option>
-            <option value="seller">Vendedor</option>
-            <option value="administrator">Administrador</option>
-          </select>
-          <Button
-            className={ `bg-green-500 rounded h-12 w-36
+          <option value="customer">Cliente</option>
+          <option value="seller">Vendedor</option>
+          <option value="administrator">Administrador</option>
+        </select>
+        <Button
+          className={ `bg-green-500 rounded h-12 w-36
           hover:bg-green-400 disabled:bg-zinc-600` }
-            type="submit"
-            data-testid="admin_manage__button-register"
-            disabled={ !formFieldsAreValid }
-          >
-            Cadastrar
-          </Button>
-        </form>
-        {(errorMessage) && (
-          <Notification
-            message={ errorMessage }
-            dataTestId="admin_manage__element-invalid-register"
-          />
-        )}
-      </div>
-    </>
+          type="submit"
+          data-testid="admin_manage__button-register"
+          disabled={ !formFieldsAreValid }
+        >
+          Cadastrar
+        </Button>
+      </form>
+      {(errorMessage) && (
+        <Notification
+          message={ errorMessage }
+          dataTestId="admin_manage__element-invalid-register"
+        />
+      )}
+    </div>
   );
 }
 
