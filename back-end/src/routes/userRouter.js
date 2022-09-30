3b63a912df3458e9ff9f098de05d1611cfb36bcd
Raphael <rafa.ramos_22@hotmail.com>
2022-09-30 16:39:53 -0300

    feat: adicionando  rota do endPoint get/user

--- back-end/src/routes/userRouter.js
+++ back-end/src/routes/userRouter.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.post('/user', (req, res) => UserController.create(req, res));
 router.get('/user/:role', (req, res) => UserController.getAll(req, res));
+router.get('/user', (req, res) => UserController.getAllUser(req, res));
 router.post(
   '/user/admin', 
   authenticationMiddleware,
