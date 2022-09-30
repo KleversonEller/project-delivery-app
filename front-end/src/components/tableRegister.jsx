b4659e5bd5732daa9108594e39a08b4d04438e2f
Raphael <rafa.ramos_22@hotmail.com>
2022-09-30 16:47:38 -0300

    feat: criando componente de tabela dos usuarios cadastrados

---
+++ front-end/src/components/tableRegister.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import requestGetAllUser from '../services/requestGetAlUser';
+
+export default function TabelaRegister() {
+  const [user, setUser] = useState([]);
+
+  const handle = async () => {
+    const result = await requestGetAllUser();
+    setUser(result);
+  };
+  useEffect(() => {
+    handle();
+  });
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Tipo</th>
+            <th>Execluir</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {user?.map((element, i) => (
+            <tr key={ i }>
+              <td
+                data-testid={ `admin_manage__element-user-table-item-number-${i}` }
+              >
+                {i + 1}
+              </td>
+              <td
+                data-testid={ `admin_manage__element-user-table-name-${i}` }
+              >
+                {element.name}
+              </td>
+              <td
+                data-testid={ `admin_manage__element-user-table-email-${i}` }
+              >
+                {element.email}
+              </td>
+              <td
+                data-testid={ `admin_manage__element-user-table-role-${i}` }
+              >
+                {element.role}
+              </td>
+              <td>
+                <button
+                  type="button"
+                  data-testid={ `admin_manage__element-user-table-remove-${i}` }
+                >
+                  Remover
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+
+    </div>
+  );
+}
